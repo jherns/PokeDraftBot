fcd1ecd74b79b3abd4abefa6a1e1c3140774fa0a
John Hernley <jdhernley@gmail.com>
2022-01-06 23:18:57 -0500

    add utils

---
+++ src/utils.js
@@ -0,0 +1,26 @@
+const fetchPokemonInfo = require('./pokeAPIWrapper/pokemonInfo.js');
+
+async function getPokemonInfo(pokemonName) {
+  const pokemonInfo = await fetchPokemonInfo(pokemonName.toLowerCase());
+  const { abilities, stats, types } = pokemonInfo;
+  const replies = [];
+  replies.push(`Pokemon: ${pokemonName}`);
+  const abilitiesList = [];
+  abilities.forEach((ability) => {
+    abilitiesList.push(ability.ability.name);
+  });
+  replies.push(`Abilities: ${abilitiesList.join(', ')}`);
+  const typeList = [];
+  types.forEach((type) => {
+    typeList.push(type.type.name);
+  });
+  replies.push(`Types: ${typeList.join(', ')}`);
+  stats.forEach((stat) => {
+    replies.push(`${stat.stat.name.toUpperCase()}: ${stat.base_stat}`);
+  });
+  return replies.join('\n');
+}
+
+module.exports = {
+  getPokemonInfo,
+};
