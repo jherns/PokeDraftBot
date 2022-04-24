a6b21f4b0cd686b79e54ff83d2b79b16d6f339d5
John Hernley <jdhernley@gmail.com>
2022-04-24 17:44:03 -0400

    update mons for season 9

--- importPokemon.js
+++ importPokemon.js
@@ -1,44 +1,12 @@
 const csvtojson = require('csvtojson');
-const mysql = require('mysql');
+const fs = require('fs');
 
 const importPokemon = (databaseCredentials) => {
-  // Establish connection to the database
-  let con = mysql.createConnection({
-    host: databaseCredentials.hostName,
-    user: databaseCredentials.username,
-    password: databaseCredentials.password,
-    database: databaseCredentials.database,
-  });
-
-  const insertMon = (insertStatement, items) => {
-    if (items[0] !== '') {
-      con.query(insertStatement, items, (err, results, fields) => {
-        if (err) {
-          // console.log('Unable to insert item at row ', i + 1);
-          return console.log(err);
-        }
-      });
-    }
-  };
-
-  con.connect((err) => {
-    if (err) return console.error('error: ' + err.message);
-
-    //con.query('DROP TABLE pokemon', () => {
-    // Query to create table "sample"
-    var createStatament =
-      'CREATE TABLE IF NOT EXISTS pokemon (id SMALLINT NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, tier VARCHAR(255) NOT NULL, PRIMARY KEY (id))';
-
-    // Creating table "sample"
-    con.query(createStatament, (err, result) => {
-      if (err) console.log('ERROR: ', err);
-    });
-    //});
-  });
-
   // CSV file name
-  const fileName = 'Season7Tiers.csv';
-
+  const fileName = 'PokemonTiers.csv';
+  let writer = fs.createWriteStream('createPokemon.sql') ;
+  writer.write('INSERT INTO pokemon (id, name, tier) VALUES ');
+  let id = 0;
   csvtojson()
     .fromFile(fileName)
     .then((source) => {
@@ -51,13 +19,24 @@ const importPokemon = (databaseCredentials) => {
         const ruMon = source[i]['RU'];
         const nuMon = source[i]['NU'];
         const puMon = source[i]['PU'];
-        var insertStatement = `INSERT INTO pokemon (name, tier) VALUES (?, ?)`;
-        insertMon(insertStatement, [ou1Mon, 'OU1']);
-        insertMon(insertStatement, [ou2Mon, 'OU2']);
-        insertMon(insertStatement, [uuMon, 'UU']);
-        insertMon(insertStatement, [ruMon, 'RU']);
-        insertMon(insertStatement, [nuMon, 'NU']);
-        insertMon(insertStatement, [puMon, 'PU']);
+        if (ou1Mon) {
+        writer.write(`(${id++},${ou1Mon},'OU1'),`);
+        }
+        if (ou2Mon) {
+        writer.write(`(${id++},${ou2Mon},'OU2'),`);
+        }
+        if (uuMon) {
+        writer.write(`(${id++},${uuMon},'UU'),`);
+        }
+        if (ruMon) {
+        writer.write(`(${id++},${ruMon},'RU'),`);
+        }
+        if (nuMon) {
+        writer.write(`(${id++},${nuMon},'NU'),`);
+        }
+        if (puMon) {
+        writer.write(`(${id++},${puMon},'PU')${i === (source.length - 1) ? ';' : ','}\n`);
+        }
       }
       console.log('All items stored into database successfully');
     });
